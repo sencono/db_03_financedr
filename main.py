3fab5fd7e15cf05cd5a7dc567cd4922ccf049a53
sencono <plm19749@gmail.com>
2026-05-26 15:55:13 +0900

    🚧Tab 추가

--- main.py
+++ main.py
@@ -182,7 +182,7 @@ def main(page: ft.Page):
     page.padding = 16
     page.window.width = 700
     page.window.height = 500
-    page.scroll = ft.ScrollMode.ADAPTIVE
+    # page.scroll = ft.ScrollMode.ADAPTIVE
     # endregion
 
     con = duckdb.connect("data/finance.db")
@@ -191,7 +191,6 @@ def main(page: ft.Page):
     add_all_assets(con)
 
     df = find_assets_by_keyword(con, None)
-
 
     # DataTable
     def create_rows(df) -> ft.DataRow:
@@ -204,7 +203,6 @@ def main(page: ft.Page):
             ) for row in df.values
         ]
 
-
     table_assets = ft.DataTable(
         columns=[
             ft.DataColumn(ft.Text(str.upper(col))) 
@@ -212,7 +210,6 @@ def main(page: ft.Page):
         ],
         rows= create_rows(df),
     )
-
 
     # Filtering
     def on_filter_change(e):
@@ -223,7 +220,6 @@ def main(page: ft.Page):
         table_assets.rows = create_rows(filtered_df)
 
         page.update()
-
 
     filter_input = ft.Container(
         ft.TextField(
@@ -237,11 +233,51 @@ def main(page: ft.Page):
         )
     )
 
-    page.add(
-        filter_input,
-        table_assets,
+    # region [탭] 종목, 계좌, 보유, 시세, Join
+    tab_assets = ft.Column(
+        expand=True,
+        scroll=ft.ScrollMode.ALWAYS,
+        controls=[filter_input, table_assets],
     )
 
+    tab_accounts = ft.Text("계좌")
+    tab_holdings = ft.Text("보유")
+    tab_prices = ft.Text("시세")
+    tab_join = ft.Text("Join")
+
+    tabs = ft.Tabs(
+        length=5,
+        expand=True,
+        content=ft.Column(
+            expand=True,
+            controls=[
+                ft.TabBar(
+                    tabs=[
+                        ft.Tab(label="종목", icon=ft.Icons.MONETIZATION_ON_OUTLINED),
+                        ft.Tab(label="계좌", icon=ft.Icons.SAVINGS_OUTLINED),
+                        ft.Tab(label="보유", icon=ft.Icons.FAVORITE_BORDER_OUTLINED),
+                        ft.Tab(label="시세", icon=ft.Icons.CANDLESTICK_CHART_OUTLINED),
+                        ft.Tab(label="Join", icon=ft.Icons.JOIN_LEFT_OUTLINED),
+                    ]
+                ),
+                ft.TabBarView(
+                    expand=True,
+                    controls=[
+                        tab_assets,
+                        tab_accounts,
+                        tab_holdings,
+                        tab_prices,
+                        tab_join,
+                    ],
+                ),
+            ],
+        ),
+    )
+    # endregion
+
+    page.add(
+        tabs,
+    )
 
 
 if __name__ == "__main__":
